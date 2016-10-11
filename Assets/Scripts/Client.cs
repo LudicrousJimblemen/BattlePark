068afd0e957a9ba4adf6807a74f6bd4f348a8810
Poyo <poyoarya@gmail.com>
2016-10-11 12:59:17 -0700

    removed stolen sanicball code, replaced with our own

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Lidgren.Network;
+
+public class Client : MonoBehaviour {
+	public NetClient client;
+	
+	void Start () {
+		client = new NetClient(new NetPeerConfiguration("Battle Park"));
+		
+		if (client.ConnectionStatus == NetConnectionStatus.Disconnected) {
+			client.Start();
+			client.Connect("127.0.0.1", 12345);
+		}
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			var message = client.CreateMessage();
+			message.Write("bep bep!!!");
+			client.SendMessage(message, NetDeliveryMethod.ReliableOrdered);
+		}
+		
+		if (client.ConnectionStatus == NetConnectionStatus.Connected) {
+			NetIncomingMessage message;
+			while ((message = client.ReadMessage()) != null)
+			{
+				switch (message.MessageType)
+				{
+					case NetIncomingMessageType.Data:
+						print("Client Data: " + message.ReadString());
+						break;
+						
+					/*case NetIncomingMessageType.StatusChanged:
+						switch(message.SenderConnection.Status)
+						{
+						}
+						break;
+					*/	
+					case NetIncomingMessageType.DebugMessage:
+						print("Client Debug: " + message.ReadString());
+						break;
+						
+						/* .. */
+					default:
+						print("unhandled message with type: " 	+ message.MessageType);
+						break;
+			    }
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Lidgren.Network;
+
+public class Server : MonoBehaviour {
+	public NetServer server;
+
+	// Use this for initialization
+	void Start () {
+		server = new NetServer(new NetPeerConfiguration("Battle Park") { Port = 12345 });
+		server.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (server.Status == NetPeerStatus.Running) {
+			NetIncomingMessage message;
+			while ((message = server.ReadMessage()) != null)
+			{
+				switch (message.MessageType)
+				{
+					case NetIncomingMessageType.Data:
+						print("Server Data: " + message.ReadString());
+						break;
+						
+					/*case NetIncomingMessageType.StatusChanged:
+						switch(message.SenderConnection.Status)
+						{
+						}
+						break;
+					*/	
+					case NetIncomingMessageType.DebugMessage:
+						print("Server Debug: " + message.ReadString());
+						break;
+						
+						/* .. */
+					default:
+						print("unhandled message with type: " 	+ message.MessageType);
+						break;
+			    }
+			}
+		}
+	}
+}
