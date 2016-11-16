d609283112b36147bf0957881e9ae9dc001dccc2
Poyo <poyoarya@gmail.com>
2016-11-15 22:24:39 -0800

    test commit!
    Discord.

--- Assets/Resources/Scripts/Grid.cs
+++ Assets/Resources/Scripts/Grid.cs
@@ -15,8 +15,6 @@ public class Grid : MonoBehaviour
 	public Dictionary<Vector3, GridObject> Objects;
 	
 	void Start() {
-		// G R I D # # #
-		// 0 1 2 3 4 5 6 etc
 		PlayerId = int.Parse(name.Substring(4, name.Length - 4));
 		Objects = new Dictionary<Vector3, GridObject> ();
 	}
