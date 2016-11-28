fc318d7579f4287c95330a779ff973ae85a7ac47
Poyo <poyoarya@gmail.com>
2016-11-27 20:43:59 -0800

    semi-dynamic text-localization

---
+++ Assets/Scripts/TextLocalizer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextLocalizer : MonoBehaviour {
+	[Serializable]
+	public struct LocalizableText {
+		public Text Text;
+		public string Reference;
+	}
+
+	public List<LocalizableText> Texts;
+
+	private void Update() {
+		foreach (var text in Texts) {
+			text.Text.text = LanguageManager.GetString(text.Reference);
+		}
+	}
+}
