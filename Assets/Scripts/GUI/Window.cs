e12d83c0ef954e41d70f3dce16ace8a42ef9fbb7
Poyo <poyoarya@gmail.com>
2016-12-07 19:22:31 -0800

    nonworking window gui

---
+++ Assets/Scripts/GUI/Window.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace BattlePark.Menu {
+	[RequireComponent(typeof(EventTrigger))]
+	public class Window : MonoBehaviour {
+		public GameObject ResizerPrefab;
+		
+		public RectTransform Target;
+		private EventTrigger eventTrigger;
+		
+		private void Awake() {
+			eventTrigger = GetComponent<EventTrigger>();
+			
+			EventTrigger.Entry entry = new EventTrigger.Entry();
+			entry.eventID = EventTriggerType.Drag;
+			entry.callback.AddListener(data => {
+				OnDrag((PointerEventData)data);
+			});
+			
+			eventTrigger.triggers.Add(entry);
+			
+			for (int i = 0; i < 4; i++) {
+				GameObject resizer = (GameObject)Instantiate(ResizerPrefab, Vector3.zero, Quaternion.identity, transform.parent);
+				Image image = resizer.GetComponent<Image>();
+				image.color = Random.ColorHSV(0, 1f, 1f, 1f, 1f, 1f, 0.75f, 0.75f);
+				
+				WindowResizer windowResizer = resizer.GetComponent<WindowResizer>();
+				windowResizer.Type = (WindowResizerType)i;
+				windowResizer.Target = Target;
+				
+				RectTransform rectTransform = resizer.GetComponent<RectTransform>();
+				
+				switch ((WindowResizerType)i) {
+					case WindowResizerType.Top:
+						rectTransform.anchorMin = new Vector2(0, 1f);
+						rectTransform.anchorMax = new Vector2(1f, 1f);
+						rectTransform.sizeDelta = new Vector2(-16f, 16f);
+						break;
+						
+					case WindowResizerType.Bottom:
+						rectTransform.anchorMin = new Vector2(0, 0f);
+						rectTransform.anchorMax = new Vector2(1f, 0f);
+						rectTransform.sizeDelta = new Vector2(-16f, 16f);
+						break;
+						
+					case WindowResizerType.Left:
+						rectTransform.anchorMin = new Vector2(0, 0f);
+						rectTransform.anchorMax = new Vector2(0f, 1f);
+						rectTransform.sizeDelta = new Vector2(16f, -16f);
+						break;
+						
+					case WindowResizerType.Right:
+						rectTransform.anchorMin = new Vector2(1, 0f);
+						rectTransform.anchorMax = new Vector2(1f, 1f);
+						rectTransform.sizeDelta = new Vector2(16f, -16f);
+						break;
+				}
+				
+				rectTransform.anchoredPosition = Vector2.zero;
+			}
+		}
+	
+		private void OnDrag(BaseEventData data) {
+			PointerEventData pointerEventData = (PointerEventData)data;
+			Target.Translate(pointerEventData.delta);
+		}
+	}
+}
---
+++ Assets/Scripts/GUI/WindowResizer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace BattlePark.Menu {
+	public enum WindowResizerType {
+		Top,
+		Bottom,
+		Left,
+		Right,
+		TopLeft,
+		TopRight,
+		BottomLeft,
+		BottomRight
+	}
+
+	[RequireComponent(typeof(EventTrigger))]
+	public class WindowResizer : MonoBehaviour {
+		public WindowResizerType Type;
+		public RectTransform Target;
+		
+		public Vector2 MinimumDimmensions;
+		public Vector2 MaximumDimmensions;
+    
+		private EventTrigger eventTrigger;
+    
+		private void Awake() {
+			eventTrigger = GetComponent<EventTrigger>();
+			EventTrigger.Entry entry = new EventTrigger.Entry();
+			entry.eventID = EventTriggerType.Drag;
+			entry.callback.AddListener(data => {
+				OnDrag((PointerEventData)data);
+			});
+			eventTrigger.triggers.Add(entry);
+		}
+
+		private void OnDrag(PointerEventData data) {
+			RectTransform.Edge? horizontalEdge = null;
+			RectTransform.Edge? verticalEdge = null;
+
+			switch (Type) {
+				case WindowResizerType.TopRight:
+					horizontalEdge = RectTransform.Edge.Left;
+					verticalEdge = RectTransform.Edge.Bottom;
+					break;
+				case WindowResizerType.Right:
+					horizontalEdge = RectTransform.Edge.Left;
+					break;
+				case WindowResizerType.BottomRight:
+					horizontalEdge = RectTransform.Edge.Left;
+					verticalEdge = RectTransform.Edge.Top;
+					break;
+				case WindowResizerType.Bottom:
+					verticalEdge = RectTransform.Edge.Top;
+					break;
+				case WindowResizerType.BottomLeft:
+					horizontalEdge = RectTransform.Edge.Right;
+					verticalEdge = RectTransform.Edge.Top;
+					break;
+				case WindowResizerType.Left:
+					horizontalEdge = RectTransform.Edge.Right;
+					break;
+				case WindowResizerType.TopLeft:
+					horizontalEdge = RectTransform.Edge.Right;
+					verticalEdge = RectTransform.Edge.Bottom;
+					break;
+				case WindowResizerType.Top:
+					verticalEdge = RectTransform.Edge.Bottom;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+			if (horizontalEdge != null) {
+				if (horizontalEdge == RectTransform.Edge.Right)
+					Target.SetInsetAndSizeFromParentEdge((RectTransform.Edge)horizontalEdge, 
+						Screen.width - Target.position.x - Target.pivot.x * Target.rect.width,
+						Mathf.Clamp(Target.rect.width - data.delta.x, MinimumDimmensions.x, MaximumDimmensions.x));
+				else
+					Target.SetInsetAndSizeFromParentEdge((RectTransform.Edge)horizontalEdge, 
+						Target.position.x - Target.pivot.x * Target.rect.width, 
+						Mathf.Clamp(Target.rect.width + data.delta.x, MinimumDimmensions.x, MaximumDimmensions.x));
+			}
+			if (verticalEdge != null) {
+				if (verticalEdge == RectTransform.Edge.Top)
+					Target.SetInsetAndSizeFromParentEdge((RectTransform.Edge)verticalEdge, 
+						Screen.height - Target.position.y - Target.pivot.y * Target.rect.height, 
+						Mathf.Clamp(Target.rect.height - data.delta.y, MinimumDimmensions.y, MaximumDimmensions.y));
+				else
+					Target.SetInsetAndSizeFromParentEdge((RectTransform.Edge)verticalEdge, 
+						Target.position.y - Target.pivot.y * Target.rect.height, 
+						Mathf.Clamp(Target.rect.height + data.delta.y, MinimumDimmensions.y, MaximumDimmensions.y));
+			}
+		}
+	}
+}
