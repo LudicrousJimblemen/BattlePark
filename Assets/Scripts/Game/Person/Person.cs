ec07e8e3e847272a827173e26207bd4c3ee670d8
Poyo <poyo_ssb@hotmail.com>
2017-02-16 20:06:38 -0800

    basic person thought

---
+++ Assets/Scripts/Game/Person/Person.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Person : MonoBehaviour {
+	public Thoughts Thoughts { get; set; }
+
+	private void Start() {
+		Thoughts.Think("person.thoughts.wantFood.ludicrous", "MINDBLOWING MACARONI");
+	}
+}
---
+++ Assets/Scripts/Game/Person/Thought.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Thought {
+	public DateTime Time { get; private set; }
+	public string ThoughtString { get; private set; }
+	public string[] Parameters { get; private set; }
+
+	public Thought(string thought, params string[] parameters) {
+		this.ThoughtString = thought;
+		this.Parameters = parameters;
+	}
+}
