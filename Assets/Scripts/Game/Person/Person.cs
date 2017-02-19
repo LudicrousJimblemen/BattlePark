c489c0a44d9f514ed68d2d38446328c72e63a7db
Avekeez <s-ssoetomo@lwsd.org>
2017-02-19 15:54:21 -0800

    fixed idiocracy, adjusted spacing

--- Assets/Scripts/Game/Person/Person.cs
+++ Assets/Scripts/Game/Person/Person.cs
@@ -7,16 +7,16 @@ using System.Linq;
 public class Person : MonoBehaviour {
 	[HideInInspector]
 	public PersonProperty[] properties = new PersonProperty[] {
-			new PersonProperty("name",			"default",			typeof(string)),
+			new PersonProperty("name","default",typeof(string)),
 			// Represents how much money a person has in cents
-			new PersonProperty("money",			0,			typeof(int),		0,      int.MaxValue),
-			new PersonProperty("hunger",		0,			typeof(float)),
-			new PersonProperty("thirst",		0,			typeof(float)),
-			new PersonProperty("nausea",		0,			typeof(float)),
-			new PersonProperty("bathroomosity",	0,			typeof(float)),
-			new PersonProperty("happiness",		100,		typeof(float)),
-			new PersonProperty("anger",			0,			typeof(float)),
-			new PersonProperty("suspicion",		0,          typeof(float))
+			new PersonProperty("money",0,typeof(int),0,int.MaxValue),
+			new PersonProperty("hunger",0,typeof(float)),
+			new PersonProperty("thirst",0,typeof(float)),
+			new PersonProperty("nausea",0,typeof(float)),
+			new PersonProperty("bathroomosity",0,typeof(float)),
+			new PersonProperty("happiness",100,typeof(float)),
+			new PersonProperty("anger",0,typeof(float)),
+			new PersonProperty("suspicion",0,typeof(float))
 		};
 
 
@@ -33,70 +33,58 @@ public class Person : MonoBehaviour {
 		SetProperty("suspicion",UnityEngine.Random.Range(0,2));
 	}
 
-	private void Awake () {
-		reroll();
-	}
-
-	private void Reset () {
+	private void Awake() {
 		reroll();
 	}
 
 	public object GetProperty(string key) {
-		for (int i = 0; i < properties.Length; i ++) {
-			//Debug.Log(properties[i].Key + ", " + key.ToLower());
+		for(int i = 0; i < properties.Length; i++) {
 			if(properties[i].Key == key.ToLower()) {
 				return properties[i].Value;
 			}
 		}
 		return null;
-		//throw new ArgumentException("Property does not exist with given key");
 	}
-	public void SetProperty(string key, object value) {
+	public void SetProperty(string key,object value) {
 		print(properties.Length);
 		for(int i = 0; i < properties.Length; i++) {
-			//Debug.Log(properties[i].Key + ", " + key.ToLower());
 			if(properties[i].Key == key.ToLower()) {
-				/*
-				if(properties[i].Value.GetType() != value.GetType())
-					throw new ArgumentException("Invalid type of value");
-				*/
 				properties[i].Value = value;
 			}
 		}
-		//throw new ArgumentException("Property does not exist with given key");
 	}
 
 	public List<Thought> Thoughts = new List<Thought>();
 
 	private void Start() {
-		Thoughts.Add(new Thought("person.thoughts.wantFood.ludicrous", "MINDBLOWING MACARONI"));
+		Thoughts.Add(new Thought("person.thoughts.wantFood.ludicrous","MINDBLOWING MACARONI"));
 	}
 
 	private static string GenerateUsername() {
 		const string consonants = "bbbbbbbbbbbbbbbbbbbbbbbcdffgghjklmnppppppppppppppppppppppqrsssstvwxzzzz";
 		const string vowels = "aaeeiiiiiooooooooooouuuuuuuuuuuuuy";
-		int type = Mathf.RoundToInt(UnityEngine.Random.Range(0, 1));
+		int type = Mathf.RoundToInt(UnityEngine.Random.Range(0,1));
 
 		string returnedName = String.Empty;
-		for (int i = 0; i < 14; i++) {
-			if (i != 7) {
+		for(int i = 0; i < 14; i++) {
+			if(i != 7) {
 				float chance = UnityEngine.Random.value;
 				bool upper = (i == 0 || returnedName[i - 1].Equals(' '));
 				string source;
-				if (type == 0) {
+				if(type == 0) {
 					source = consonants;
-					if (upper)
+					if(upper)
 						source = source.ToUpper();
-					returnedName += source.ElementAt(UnityEngine.Random.Range(0, consonants.Length));
-					if (chance <= 0.5) {
+					returnedName += source.ElementAt(UnityEngine.Random.Range(0,consonants.Length));
+					if(chance <= 0.5) {
 						type = 1;
 					}
 				} else {
 					source = vowels;
-					if (upper)
+					if(upper)
 						source = source.ToUpper();
-					returnedName += source.ElementAt(UnityEngine.Random.Range(0, vowels.Length));
-					if (chance <= 0.6) {
+					returnedName += source.ElementAt(UnityEngine.Random.Range(0,vowels.Length));
+					if(chance <= 0.6) {
 						type = 0;
 					}
 				}
