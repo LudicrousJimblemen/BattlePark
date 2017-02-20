02099ee7797e02bbe124826c7d6735dbb9937d27
Poyo <poyo_ssb@hotmail.com>
2017-02-20 15:15:57 -0800

    fix sculpture hitbox (fixes #10)

--- Assets/Scripts/Game/Grid/GridObjects/GridSculpture.cs
+++ Assets/Scripts/Game/Grid/GridObjects/GridSculpture.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 
 public class GridSculpture : GridObject {
-	public override Vector3[] OccupiedOffsets {	get { return new[] {
-		Vector3.zero,
-		new Vector3 (0,0,1),
-		new Vector3 (0,0,2),
-		new Vector3 (0,0,3),
-		new Vector3 (1,0,0),
-		new Vector3 (2,0,0),
-		new Vector3 (3,0,0)
-	};}}
+	public override Vector3[] OccupiedOffsets {
+		get {
+			return new[] {
+				Vector3.zero,
+				new Vector3(0, 1, 0)
+			};
+		}
+	}
 }
