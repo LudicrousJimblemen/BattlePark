7975d37d218aab38601df1b78ad2835a0b146c8f
Poyo <poyo_ssb@hotmail.com>
2017-02-20 18:51:13 -0800

    fixed recursion bug #14

--- Assets/Scripts/Game/Grid/GridObjects/GridPath.cs
+++ Assets/Scripts/Game/Grid/GridObjects/GridPath.cs
@@ -25,6 +25,13 @@ public class GridPath : GridObject {
 		base.OnPlaced();
 
 		UpdateMesh();
+		foreach (var gridObject in Grid.Instance.Objects.AdjacentObjects(this.GridPosition, true)) {
+			// "as" keyword returns null if cast is invalid
+			GridPath path = gridObject as GridPath;
+			if (path != null) {
+				path.UpdateMesh();
+			}
+		}
 		
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 2; j++) {
@@ -36,14 +43,6 @@ public class GridPath : GridObject {
 	}
 
 	public void UpdateMesh() {
-		foreach (var gridObject in Grid.Instance.Objects.AdjacentObjects(this.GridPosition, true)) {
-			// "as" keyword returns null if cast is invalid
-			GridPath path = gridObject as GridPath;
-			if (path != null) {
-				path.UpdateMesh();
-			}
-		}
-
 		EdgeSouth.SetActive(Grid.Instance.Objects.ObjectAt(this.GridPosition + new Vector3(0, 0, -1)) as GridPath != null);
 		EdgeNorth.SetActive(Grid.Instance.Objects.ObjectAt(this.GridPosition + new Vector3(0, 0, 1)) as GridPath != null);
 		EdgeWest.SetActive(Grid.Instance.Objects.ObjectAt(this.GridPosition + new Vector3(-1, 0, 0)) as GridPath != null);
