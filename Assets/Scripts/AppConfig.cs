8d26b545e405ecddfe48d96925dc1fc9339ac6ef
Poyo <poyo_ssb@hotmail.com>
2017-03-06 15:37:03 -0800

    delet everything

--- Assets/Scripts/AppConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace BattlePark.Core {
-	public static class AppConfig {
-		public static string Name = "Battle Park";
-		
-		public static AppVersion Version = new AppVersion(0, 2, 1);
-	}
-}
