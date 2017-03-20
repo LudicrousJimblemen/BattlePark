ecb743bed648459a51d84ba5c92980ccd7ccf5ed
Poyo <poyo_ssb@hotmail.com>
2017-03-20 11:29:25 -0700

    draggable windows

---
+++ Assets/Scripts/GUI/Window/Window.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Window : MonoBehaviour {
+	public Image TitleBar;
+	public Text TitleText;
+}
---
+++ Assets/Scripts/GUI/Window/WindowTitleBar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class WindowTitleBar : MonoBehaviour {
+	private readonly float minX;
+	private readonly float maxX = Screen.width;
+	private readonly float minY;
+	private readonly float maxY = Screen.height;
+ 
+	private Vector3 lastMousePosition;
+	private RectTransform rectTransform;
+	
+	private void Awake() {
+		rectTransform = transform.parent.GetComponent<RectTransform>();
+	}
+ 
+	private Vector3 GetClampedMousePosition() {
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.x = Mathf.Clamp(mousePosition.x, 0f, Screen.width);
+		mousePosition.y = Mathf.Clamp(mousePosition.y, 0f, Screen.height);
+ 
+		return mousePosition;
+	}
+ 
+	public void OnMouseDown() {
+		lastMousePosition = GetClampedMousePosition();
+	}
+ 
+	public void OnMouseDrag() {
+		transform.parent.position += GetClampedMousePosition() - lastMousePosition;
+ 
+		Vector3 position = transform.parent.position;
+		position.x = Mathf.Clamp(position.x, minX, maxX);
+		position.y = Mathf.Clamp(position.y, minY, maxY);
+ 
+		transform.parent.position = position;
+ 
+		lastMousePosition = GetClampedMousePosition();
+	}
+}
