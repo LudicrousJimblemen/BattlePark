80933fdca85fec609df461352ada37ed1739e00c
Poyo <poyoarya@gmail.com>
2017-05-07 22:12:11 -0700

    more scenes bodged

--- Assets/Scripts/Bodge/BodgeManager.cs
+++ Assets/Scripts/Bodge/BodgeManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.UI;
 
 public class BodgeManager : MonoBehaviour {
 	public static BodgeManager Instance;
@@ -16,22 +17,27 @@ public class BodgeManager : MonoBehaviour {
 
 	public GridObject[] Objects;
 	public List<Person> Guests;
-	
+
 	[Header("BuildRollercoaster")]
 	public GameObject Coaster;
-	
+
 	[Header("Infuse")]
+	public Text Text;
 	public GameObject Macaroni;
 	public GameObject Dynamite;
 	public GameObject Dynamacaroni;
-	
+
+	[Header("LaunchExplosion")]
+	public GameObject RidingPerson;
+	public GameObject LaunchedPerson;
+
 	private void Awake() {
-		if(FindObjectsOfType<BodgeManager>().Length > 1) {
+		if (FindObjectsOfType<BodgeManager>().Length > 1) {
 			Destroy(gameObject);
 			return;
 		}
 		Instance = this;
-		
+
 		if (Application.loadedLevelName == "PostIntro") {
 			StartCoroutine(PostIntro());
 		}
@@ -44,51 +50,84 @@ public class BodgeManager : MonoBehaviour {
 		if (Application.loadedLevelName == "Infuse") {
 			StartCoroutine(Infuse());
 		}
+		if (Application.loadedLevelName == "TwoParks") {
+			StartCoroutine(TwoParks());
+		}
+		if (Application.loadedLevelName == "LaunchExplosion") {
+			StartCoroutine(LaunchExplosion());
+		}
 	}
-	
+
 	private IEnumerator PostIntro() {
 		for (int i = 0; i < 90; i++) {
 			Instantiate(PersonObj, new Vector3(-4.85f, 0, 0), Quaternion.identity);
 			yield return new WaitForSeconds(0.05f);
 		}
 	}
-	
+
 	private IEnumerator BuildRollercoaster() {
 		List<GameObject> coasterParts = new List<GameObject>();
 		for (int i = 0; i < Coaster.transform.childCount; i++) {
 			coasterParts.Add(Coaster.transform.GetChild(i).gameObject);
 		}
-		
+
 		foreach (var part in coasterParts) {
 			part.SetActive(false);
 		}
-		
+
 		for (int i = 0; i < 90; i++) {
 			Instantiate(PersonObj, new Vector3(-4.85f, 0, 0), Quaternion.identity);
 		}
-		
+
 		yield return new WaitForSeconds(5f);
-		
+
 		foreach (var part in coasterParts) {
 			part.SetActive(true);
 			yield return new WaitForSeconds(0.5f);
 		}
 	}
-	
+
 	private IEnumerator Infuse() {
 		Dynamacaroni.SetActive(false);
 		for (int i = 0; i < 50; i++) {
 			Instantiate(PersonObj, new Vector3(-4.85f, 0, 0), Quaternion.identity);
 			yield return new WaitForSeconds(0.05f);
 		}
-		
+
 		yield return new WaitForSeconds(12f);
 		Dynamacaroni.SetActive(true);
 		yield return new WaitForSeconds(0.4f);
+		Text.text = "Infused!";
 		Dynamite.SetActive(false);
 		Macaroni.SetActive(false);
 	}
-	
+
+	private IEnumerator TwoParks() {
+		for (int i = 0; i < 70; i++) {
+			Instantiate(PersonObj, new Vector3(-4.85f, 0, 0), Quaternion.identity).GetComponent<BodgePerson>().Walker.graph = Graphs[0];
+		}
+		for (int i = 0; i < 50; i++) {
+			Instantiate(PersonObj, new Vector3(4.85f, 0, 0), Quaternion.identity).GetComponent<BodgePerson>().Walker.graph = Graphs[1];
+		}
+		yield return null;
+	}
+
+	private IEnumerator LaunchExplosion() {
+		LaunchedPerson.SetActive(false);
+		for (int i = 0; i < 60; i++) {
+			Instantiate(PersonObj, new Vector3(-4.85f, 0, 0), Quaternion.identity).GetComponent<BodgePerson>().Walker.graph = Graphs[0];
+		}
+		for (int i = 0; i < 60; i++) {
+			Instantiate(PersonObj, new Vector3(4.85f, 0, 0), Quaternion.identity).GetComponent<BodgePerson>().Walker.graph = Graphs[1];
+		}
+		yield return new WaitForSeconds(18.5f - (1f / 60f));
+		RidingPerson.SetActive(false);
+		LaunchedPerson.SetActive(true);
+		LaunchedPerson.GetComponent<Rigidbody>().AddForce(new Vector3(-12f, 12f, 0f), ForceMode.VelocityChange);
+		LaunchedPerson.GetComponent<Rigidbody>().AddTorque(new Vector3(-2f, 2f, 0f), ForceMode.VelocityChange);
+		yield return new WaitForSeconds(19f / 12f);
+	}
+
 	private void OnGUI() {
 		if (Application.isEditor) {
 			GUI.Label(new Rect(12f, 12f, 200f, 200f), (Time.frameCount * Time.timeScale).ToString());
--- Assets/Scripts/Bodge/BodgePerson.cs
+++ Assets/Scripts/Bodge/BodgePerson.cs
@@ -5,15 +5,15 @@ using Pathfinding;
 
 public class BodgePerson : MonoBehaviour {
 	public PathWalker Walker;
-	
+
 	public Color Red;
 	public Color Blue;
-	
+
 	[Header("Display")]
 	public GameObject Macaroni;
 	public GameObject Stetson;
 	public GameObject SaltLick;
-	
+
 	private void Awake() {
 		Walker = GetComponent<PathWalker>();
 	}
@@ -29,9 +29,11 @@ public class BodgePerson : MonoBehaviour {
 			StartCoroutine(PostIntro());
 		} else if (Application.loadedLevelName == "Infuse") {
 			StartCoroutine(PostIntro());
+		} else if (Application.loadedLevelName == "TwoParks") {
+			StartCoroutine(TwoParks());
 		}
 	}
-	
+
 	private IEnumerator Intro() {
 		GetComponentInChildren<SkinnedMeshRenderer>().material.color = Blue;
 		Walker.SetDestination(new Vector3(-12.5f, 0, -6f));
@@ -43,7 +45,7 @@ public class BodgePerson : MonoBehaviour {
 		yield return new WaitForSeconds(0.3f);
 		Walker.SetDestination(new Vector3(-8.5f, 0, 6f));
 	}
-	
+
 	private IEnumerator PostIntro() {
 		GetComponentInChildren<SkinnedMeshRenderer>().material.color = Blue;
 		Walker.graph = BodgeManager.Instance.Graphs[0];
@@ -56,7 +58,7 @@ public class BodgePerson : MonoBehaviour {
 				} else {
 					Walker.SetDestination(new Vector3(-30.5f, 0, -6f));
 				}
-				
+
 				yield return new WaitUntil(() => Walker.DestinationReached);
 				yield return new WaitForSeconds(0.3f);
 				Macaroni.SetActive(true);
@@ -79,6 +81,82 @@ public class BodgePerson : MonoBehaviour {
 				yield return new WaitForSeconds(0.6f);
 			}
 			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+	private IEnumerator TwoParks() {
+		bool side = transform.position.x < 0;
+
+		if (side) {
+			GetComponentInChildren<SkinnedMeshRenderer>().material.color = Blue;
+		} else {
+			GetComponentInChildren<SkinnedMeshRenderer>().material.color = Red;
+		}
+		while (true) {
+			Walker.DestinationReached = false;
+			if (side) {
+				int random = Random.Range(0, 14);
+				if (random == 0 && !Macaroni.activeSelf) {
+					if (Random.value < 0.5f) {
+						Walker.SetDestination(new Vector3(-11.5f, 0, -4f));
+					} else {
+						Walker.SetDestination(new Vector3(-30.5f, 0, -6f));
+					}
+
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.3f);
+					Macaroni.SetActive(true);
+					GetComponentInChildren<ParticleSystem>().Emit(25);
+				} else if (random == 1 && !SaltLick.activeSelf) {
+					Walker.SetDestination(new Vector3(-26.5f, 0, -10f));
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.3f);
+					SaltLick.SetActive(true);
+					GetComponentInChildren<ParticleSystem>().Emit(25);
+				} else if (random == 2 && !Stetson.activeSelf) {
+					Walker.SetDestination(new Vector3(-22.5f, 0, -6f));
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.3f);
+					Stetson.SetActive(true);
+					GetComponentInChildren<ParticleSystem>().Emit(25);
+				} else {
+					Walker.SetDestination(Walker.graph.Nodes.ElementAt(Random.Range(0, Walker.graph.Nodes.Count)).Key);
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.6f);
+				}
+				yield return new WaitForSeconds(0.5f);
+			} else {
+				int random = Random.Range(0, 14);
+				if (random == 0 && !Macaroni.activeSelf) {
+					if (Random.value < 0.5f) {
+						Walker.SetDestination(new Vector3(27.5f, 0, -3f));
+					} else {
+						Walker.SetDestination(new Vector3(27.5f, 0, -6f));
+					}
+
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.3f);
+					Macaroni.SetActive(true);
+					GetComponentInChildren<ParticleSystem>().Emit(25);
+				} else if (random == 1 && !SaltLick.activeSelf) {
+					Walker.SetDestination(new Vector3(11.5f, 0, 1f));
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.3f);
+					SaltLick.SetActive(true);
+					GetComponentInChildren<ParticleSystem>().Emit(25);
+				} else if (random == 2 && !Stetson.activeSelf) {
+					Walker.SetDestination(new Vector3(20.5f, 0, 1f));
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.3f);
+					Stetson.SetActive(true);
+					GetComponentInChildren<ParticleSystem>().Emit(25);
+				} else {
+					Walker.SetDestination(Walker.graph.Nodes.ElementAt(Random.Range(0, Walker.graph.Nodes.Count)).Key);
+					yield return new WaitUntil(() => Walker.DestinationReached);
+					yield return new WaitForSeconds(0.6f);
+				}
+				yield return new WaitForSeconds(0.5f);
+			}
 		}
 	}
 }
